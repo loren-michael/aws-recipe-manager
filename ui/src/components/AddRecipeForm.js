2eeea0bc0ed6998de0e9f79235e6b76229b8e354
Loren Hartman <lorenmichaelh@gmail.com>
2024-05-23 10:54:19 -0500

    adding component files with starter code

---
+++ ui/src/components/AddRecipeForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AddRecipeForm = () => {
+  return (
+    <div>AddRecipeForm</div>
+  )
+}
---
+++ ui/src/components/UpdateRecipeForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const UpdateRecipeForm = () => {
+  return (
+    <div>UpdateRecipeForm</div>
+  )
+}
